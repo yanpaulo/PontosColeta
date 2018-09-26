8215d4583bc67fd3f10452874fa565880e77f8bf
Yan Paulo <yanpaulo@hotmail.com>
2018-09-25 23:28:10 -0300

    Fancy Map class.

--- PontosColeta.WebApp/Scripts/app/places/edit-place.ts
+++ PontosColeta.WebApp/Scripts/app/places/edit-place.ts
@@ -2,30 +2,45 @@
 import { Place } from './Place';
 import 'bingmaps';
 
-let map: Microsoft.Maps.Map;
+class Application {
+    map: Microsoft.Maps.Map;
 
-const vue = new Vue({
-    el: "#app",
-    data: {
-        place: new Place()
-    },
-    methods: {
-        submit(event: Event) {
-            if (!$(event.srcElement as Element).valid()) return;
-            console.log(this.place);
+    vue = new Vue({
+        el: "#app",
+        data: {
+            place: new Place()
+        },
+        methods: {
+            submit(event: Event) {
+                if (!$(event.srcElement as Element).valid()) return;
+                console.log(this.place);
+            }
+        },
+        created: () => { this.loadMap() }
+    });
+
+    loadMap() {
+        try {
+            this.map = new Microsoft.Maps.Map("#map", {});
+            Microsoft.Maps.Events.addHandler(this.map, "click", this.mapClicked);
+            Microsoft.Maps.loadModule('Microsoft.Maps.WellKnownText', {});
+        } catch (e) {
+            console.log(e);
+            setTimeout(() => this.loadMap(), 500);
         }
-    },
-    created: () => {
-        loadMap();
     }
-});
 
-function loadMap() {
-    try {
-        map = new Microsoft.Maps.Map("#map", {});
-    } catch (e) {
-        console.log(e);
-        setTimeout(loadMap, 500);
+    mapClicked(args: Microsoft.Maps.IMouseEventArgs) {
+        console.log(args.location);
+        console.log(args.point);
+
+        try {
+            let loc = new Microsoft.Maps.Pushpin(args.location);
+            console.log(Microsoft.Maps.WellKnownText.write(loc));
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
 
+let app = new Application();
