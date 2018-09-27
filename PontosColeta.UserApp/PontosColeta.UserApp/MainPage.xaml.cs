12c8d27bc20c88186e4b39db41abe97a34db0420
Yan Paulo <yanpaulo@hotmail.com>
2018-09-26 23:44:54 -0300

    Add TabbedPages

--- PontosColeta.UserApp/PontosColeta.UserApp/MainPage.xaml.cs
+++ PontosColeta.UserApp/PontosColeta.UserApp/MainPage.xaml.cs
@@ -4,22 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace PontosColeta.UserApp
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-
-        private async void ContentPage_Appearing(object sender, EventArgs e)
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage ()
         {
-            var locator = CrossGeolocator.Current;
-            var last = await locator.GetLastKnownLocationAsync();
-            var next = await locator.GetPositionAsync();
+            InitializeComponent();
         }
     }
 }
